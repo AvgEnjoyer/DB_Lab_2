1d0d2813d504aaced6197df52c94be46581f9c4d
AvgEnjoyer <yariktkachenko83@gmail.com>
2022-05-23 23:22:36 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DB_Lab_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
